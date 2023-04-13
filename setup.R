7c0846fc1f852fe2c318535957bafc07a230cc33
Tom Palmer <remlapmot@hotmail.com>
2023-04-13 11:44:46 +0100

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,6 +1,8 @@
-# install renv version 0.16.0 (released on 29/09/2022)
-install.packages('renv', repos = 'https://packagemanager.posit.co/cran/2022-09-30')
+if (!requireNamespace('renv', quietly = TRUE)) {
+  # install renv version 0.16.0 (released on 29/09/2022)
+  install.packages('renv', repos = 'https://packagemanager.posit.co/cran/2022-09-30')
 
-renv::init(bare = TRUE)
+  renv::init(bare = TRUE)
+}
 
 renv::restore(prompt = FALSE)
