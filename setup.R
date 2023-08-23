bf8871d81312a45eee6e82095652dc2eb20655bb
Tom Palmer <remlapmot@hotmail.com>
2023-08-23 19:35:06 +0100

    Force installation of DHARMa and BayesianTools

--- setup.R
+++ setup.R
@@ -5,4 +5,12 @@ if (!requireNamespace('renv', quietly = TRUE)) {
   renv::init(bare = TRUE)
 }
 
+# Note: 2023-08-22 renv::restore() failed, so had to force install the following
+if (!requireNamespace('DHARMa', quietly = TRUE)) {
+  renv::install("DHARMa", repos = "https://packagemanager.posit.co/cran/2022-09-09")
+}
+if (!requireNamespace('BayesianTools', quietly = TRUE)) {
+  renv::install("BayesianTools", repos = "https://packagemanager.posit.co/cran/2023-08-21")
+}
+
 renv::restore(prompt = FALSE)
