aa5bc5e5dc9864aaa4ea371ddec2d49de7b12548
Tom Palmer <remlapmot@hotmail.com>
2023-10-09 13:32:56 +0100

    Delete whitespace

--- setup.R
+++ setup.R
@@ -1,7 +1,6 @@
 if (!requireNamespace('renv', quietly = TRUE)) {
   # install renv version 0.16.0 (released on 29/09/2022)
   install.packages('renv', repos = 'https://packagemanager.posit.co/cran/2022-09-30')
-
   renv::init(bare = TRUE)
 }
 
