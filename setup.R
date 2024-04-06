6c27b5c5e11e8ce8b03b5598e9a5b11ecce450ff
Tom Palmer <remlapmot@hotmail.com>
2024-04-06 19:49:21 +0100

    Add prompt = FALSE to renv::install() calls

--- setup.R
+++ setup.R
@@ -1,15 +1,15 @@
 if (!requireNamespace('renv', quietly = TRUE)) {
   # install renv version 0.16.0 (released on 29/09/2022)
-  install.packages('renv', repos = 'https://packagemanager.posit.co/cran/2022-09-30')
+  install.packages('renv', repos = 'https://packagemanager.posit.co/cran/2022-09-30', prompt = FALSE)
   renv::init(bare = TRUE)
 }
 
 # Note: 2023-08-22 renv::restore() failed, so had to force install the following
 if (!requireNamespace('DHARMa', quietly = TRUE)) {
-  renv::install("DHARMa", repos = "https://packagemanager.posit.co/cran/2022-09-09")
+  renv::install("DHARMa", repos = "https://packagemanager.posit.co/cran/2022-09-09", prompt = FALSE)
 }
 if (!requireNamespace('BayesianTools', quietly = TRUE)) {
-  renv::install("BayesianTools", repos = "https://packagemanager.posit.co/cran/2023-08-21")
+  renv::install("BayesianTools", repos = "https://packagemanager.posit.co/cran/2023-08-21", prompt = FALSE)
 }
 
 renv::restore(prompt = FALSE)
