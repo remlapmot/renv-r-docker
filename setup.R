0dd32548d49c31d212c89ac55a517fb4bcf13b79
Tom Palmer <remlapmot@hotmail.com>
2024-04-06 19:14:21 +0100

    Remove websocket, chromote, AsioHeaders hack

--- setup.R
+++ setup.R
@@ -12,12 +12,4 @@ if (!requireNamespace('BayesianTools', quietly = TRUE)) {
   renv::install("BayesianTools", repos = "https://packagemanager.posit.co/cran/2023-08-21")
 }
 
-# Note: 2023-10-09 websocket 1.3.2, chromote 0.1.2, AsioHeaders 1.22.1-2, webshot2 0.1.1
-if (!requireNamespace('websocket', quietly = TRUE)) {
-  renv::install('websocket', repos = 'https://packagemanager.posit.co/cran/2021-04-22', type = 'source')
-}
-if (!requireNamespace('chromote', quietly = TRUE)) {
-  renv::install('chromote', repos = 'https://packagemanager.posit.co/cran/2023-08-14')
-}
-
 renv::restore(prompt = FALSE)
