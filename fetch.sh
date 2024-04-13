f312530ea47e6fc2bb75c3bc103239dbf4a076fe
Tom Palmer <remlapmot@hotmail.com>
2024-04-13 19:35:46 +0100

    Amend master to main

--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl https://raw.githubusercontent.com/opensafely-core/r-docker/master/renv.lock -O
+curl https://raw.githubusercontent.com/opensafely-core/r-docker/main/renv.lock -O
