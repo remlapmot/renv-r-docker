a56d44ff5b189eeb57277476acb284620f97e4c3
Tom Palmer <remlapmot@hotmail.com>
2024-04-14 19:46:47 +0100

    Deleting packages which were installed then removed

--- restore.R
+++ restore.R
@@ -1,1 +1,7 @@
 renv::restore(prompt = FALSE)
+
+# Due to packages which were installed then removed
+# renv::remove('pak')
+# renv::remove('webshot2')
+# renv::remove('tzdb')
+# renv::remove('DiagrammeR')
