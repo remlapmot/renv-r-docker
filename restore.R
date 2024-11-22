5e67c55ce2cdd3197ec66cae14e4711c80aa67c0
Tom Palmer <remlapmot@hotmail.com>
2024-11-22 16:00:01 +0000

    Tricks for dd4d

--- restore.R
+++ restore.R
@@ -1,5 +1,6 @@
 Sys.setenv(RENV_CONFIG_MRAN_ENABLED = FALSE)
-renv::restore(prompt = FALSE)
+renv::restore(prompt = FALSE, exclude = 'dd4d')
+install.packages('dd4d', repos = c('https://remlapmot.r-universe.dev', 'https://cloud.r-project.org'))
 
 # Due to packages which were installed then removed
 # renv::remove('pak')
