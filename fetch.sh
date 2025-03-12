c8a542692cb64187b2af8f19c9acdc745a10a9a9
Tom Palmer <remlapmot@hotmail.com>
2025-03-12 10:58:12 +0000

    Amend filename

--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl https://raw.githubusercontent.com/opensafely-core/r-docker/main/renv.lock -O
+curl https://raw.githubusercontent.com/opensafely-core/r-docker/main/v1/renv.lock -O
